5c63c3d2bad8de3608022103db5fb89e199377ad
nachitoderacing <166016638+nachitoderacing@users.noreply.github.com>
2024-07-01 07:45:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+for (let index = 10; index > 0 ; index = index -1) {
+    console.log(index)
+
+ }
+
+ for (let index = 15; index > 4 ; index = index -1) {
+     console.log(index)
+
+  }
+
+
+ for (let index = 1; index <101 ; index = index + 2) {
+     console.log(index)
+
+  }
+
+
+for (let index = 7; index < 71 ; index = index + 7) {
+    console.log(index)
+
+ }
